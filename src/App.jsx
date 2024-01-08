7f06aceb2eeca5797713bcf4d4524d72dfda3482
Lukasz <lukkotecki@gmail.com>
2024-01-08 15:06:32 +0100

    add files via CLI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() { 
+
+  return (
+    <>
+      <div className='container'>
+        To jest jakas zawartosc
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NewGameButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NewGameButton(){
+    return (
+        <button>New Game</button>
+    )
+}
